6d7e835da31accae9d15eeca5c32a04399d6861f
Diaz1620 <77040700+Diaz1620@users.noreply.github.com>
2022-10-16 14:02:24 -0400

    adjusted the model and controller

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -1,26 +1,23 @@
-from readline import insert_text
 from flask_app import app
 from flask import render_template, request, redirect
-from user import User
+from flask_app.models.user import User
 
 @app.route("/")
 def index():
-    mysql = connectToMySQL('users')	        # call the function, passing in the name of our db
-    users = mysql.query_db('SELECT * FROM users;')  # call the query_db function, pass in the query as a string
-    print(users)
-    return render_template("read(all).html", all_users = users)
+    return render_template("index.html")
+
+@app.route("/read_all")
+def read_all():
+    return render_template("read(all).html", all_users = User.retrieve_all())
 
 @app.route('/create_user', methods=['POST'])
 def add_user_to_db():
-    mysql = connectToMySQL("users")
-
-    query = "INSERT INTO users (first_name, last_name, email, created_at, updated_at) VALUES (%(fn)s, %(ln)s, %(email)s, NOW(), NOW());"
     data = {
         "fn": request.form["fname"],
         "ln": request.form["lname"],
         "email": request.form["email"]
     }
-    new_user_id = mysql.query_db(query,data)
+    User.save(data)
     return redirect("/")
 
 @app.route('/create_new')
@@ -29,44 +26,33 @@ def create_new_user():
 
 @app.route('/read_one/<int:id>')
 def read_one_page(id):
-    query = "SELECT first_name, last_name, email, created_at, updated_at FROM users WHERE id = %(id)s"
     data = {
         'id': id
     }
-    db = connectToMySQL("users")
-    bd = db.query_db(query,data)
-    return render_template("read(one).html", one_user = bd)
+    return render_template("read(one).html", one_user = User.retrieve_one(data))
 
 @app.route('/edit_form/<int:id>')
 def editing_form(id):
-    query = "SELECT id FROM users WHERE id = %(id)s"
     data = {
         'id':id
     }
-    one_person = connectToMySQL('users')
-    base = one_person.query_db(query,data)
-    return render_template("/user_edit.html", that_one_id = base)
+    return render_template("/user_edit.html", that_one_id = User.retrieve_one(data))
 
 @app.route('/users_edit/<int:id>',methods = ['POST'])
 def user_edit(id):
-    query = "UPDATE users SET first_name=%(fn)s, last_name=%(ln)s, email=%(email)s WHERE id = %(id)s"
     data = {
         "id": id,
         "fn": request.form["fname"],
         "ln": request.form["lname"],
         "email": request.form["email"]
     }
-    mysql = connectToMySQL('users')
-    mysql.query_db(query,data)
-    
+    User.update(data)
     return redirect(f'/read_one/{id}')
 
 @app.route('/delete_user/<int:id>')
 def delete_user(id):
-    query = "DELETE FROM users WHERE id = %(id)s"
     data = {
         'id': id
     }
-    one_person =connectToMySQL('users')
-    base = one_person.query_db(query,data)
+    User.destroy(data)
     return redirect('/')
